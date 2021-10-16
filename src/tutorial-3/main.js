6104444a647f82a9ea280a20c34624385ef04524
frontlake <vladimirozerskiy@gmail.com>
2021-10-16 16:32:00 +0300

    Поправил баг с удалением задачи из массива

--- src/tutorial-3/main.js
+++ src/tutorial-3/main.js
@@ -8,7 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const createElement = (text) => {
     const listElement = document.createElement('li');
     listElement.className = 'todo-list__item';
-    listElement.innerHTML = `${text}<button class="removeBtn">x</button>`;
+    listElement.innerHTML = `
+      ${text}
+      <button class="removeBtn">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </button>`;
     return listElement;
   };
 
@@ -37,6 +43,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const removeTodo = (e) => {
     if (e.target.className === 'removeBtn') {
+      const todoText = e.target.parentElement.textContent.trim();
+      listArray.forEach((item, index) => {
+        if (todoText === item) {
+          listArray.splice(index, 1);
+        }
+      });
       e.target.parentElement.remove();
     }
   };
