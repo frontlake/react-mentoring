db1ea3272da0e256e291513ea1167fc641b5a446
frontlake <vladimirozerskiy@gmail.com>
2021-10-26 21:56:44 +0300

    Сделал ДЗ-8

---
+++ src/tutorial-8/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function App() {
+  const [users, setUsers] = React.useState([]);
+
+  const url = 'https://61784062bb979200171ff624.mockapi.io/usrs';
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(url);
+      const users = await response.json();
+      setUsers(users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {users !== 'Not found' && users.length > 0 ? (
+          users.map((user) => <li key={user.id}>{user.name}</li>)
+        ) : (
+          <li>нет данных</li>
+        )}
+      </ul>
+      <button onClick={getUsers}>Получить спискок пользователей</button>
+    </div>
+  );
+}
