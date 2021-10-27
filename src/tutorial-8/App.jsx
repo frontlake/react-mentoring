3e3a4b582b492f07b8fa23c0d646450752559eb0
frontlake <vladimirozerskiy@gmail.com>
2021-10-27 23:34:56 +0300

    Доработал вывод ошибки в интерфейс

--- src/tutorial-8/App.jsx
+++ src/tutorial-8/App.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 export default function App() {
   const [users, setUsers] = React.useState([]);
+  const [error, setError] = React.useState(false);
 
   const url = 'https://61784062bb979200171ff624.mockapi.io/users';
 
   const getUsers = async () => {
     const response = await fetch(url);
-    const users = await response.json();
-    setUsers(users);
+    if (response.ok) {
+      const users = await response.json();
+      setUsers(users);
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <div>
+      <h2>Список пользователей</h2>
       <ul>
-        {users === 'Not found' ? (
-          <li>{users} - не удалось получить ответ от сервера</li>
+        {error ? (
+          <li>Что-то пошло не так...</li>
         ) : (
           users.map((user) => {
             return <li key={user.id}>{user.name}</li>;
