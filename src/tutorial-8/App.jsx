7cf2f77a4784fdf0013fde965d91db49c40cc7a9
frontlake <vladimirozerskiy@gmail.com>
2021-10-27 21:26:39 +0300

    Доработал по замечаниям

--- src/tutorial-8/App.jsx
+++ src/tutorial-8/App.jsx
@@ -6,22 +6,20 @@ export default function App() {
   const url = 'https://61784062bb979200171ff624.mockapi.io/users';
 
   const getUsers = async () => {
-    try {
-      const response = await fetch(url);
-      const users = await response.json();
-      setUsers(users);
-    } catch (error) {
-      console.log(error);
-    }
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
   };
 
   return (
     <div>
       <ul>
-        {users !== 'Not found' && users.length > 0 ? (
-          users.map((user) => <li key={user.id}>{user.name}</li>)
+        {users === 'Not found' ? (
+          <li>{users} - не удалось получить ответ от сервера</li>
         ) : (
-          <li>нет данных</li>
+          users.map((user) => {
+            return <li key={user.id}>{user.name}</li>;
+          })
         )}
       </ul>
       <button onClick={getUsers}>Получить спискок пользователей</button>
